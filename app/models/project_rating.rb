eab1dc0a13ace16cc128bdff6f2a9d7a512c65a6
pete2786 <pete2786@umn.edu>
2013-08-26 22:19:45 -0500

    Added output of volunteers.

--- app/models/project_rating.rb
+++ app/models/project_rating.rb
@@ -1,3 +1,6 @@
 class ProjectRating < ActiveRecord::Base
   attr_accessible :project_id, :rating, :user_id
+  
+  belongs_to :project
+  belongs_to :user
 end
--- app/models/project_volunteer.rb
+++ app/models/project_volunteer.rb
@@ -1,3 +1,6 @@
 class ProjectVolunteer < ActiveRecord::Base
   attr_accessible :interest_level, :project_id, :user_id
+  
+  belongs_to :project
+  belongs_to :user
 end
