5ae799a8cce0b340e4a6c77bde4ce4753a0cd3e4
David Peterson <pete2786@umn.edu>
2014-02-24 16:12:38 -0600

    Changed provider config to yaml file and deleted old token from Google api.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
-if Rails.env.development? then
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :google_oauth2, '767649200826.apps.googleusercontent.com', '3lsEI96bMAbLe1aHj1xbG10J', {:client_options => {:ssl => {:ca_path => "/usr/lib/ssl/certs"}}, :setup => true}
+config = YAML::load(File.open(Rails.root.join "config", "providers.yml"))  
+if config && !config.empty?  
+  Rails.application.config.middleware.use OmniAuth::Builder do   
+    if Rails.env.development?    then
+      OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    end    
+
+    provider :google_oauth2, config["providers"]["google_oauth2"]["key"], config["providers"]["google_oauth2"]["secret"], {:client_options => {:ssl => {:ca_path => "/usr/lib/ssl/certs"}}, :setup => true} 
   end  
-else
-  
-end
+end 
