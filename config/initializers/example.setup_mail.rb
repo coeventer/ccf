dd66dda89ec03568e72ac7a0b263d5b597c5170b
David Peterson <pete2786@umn.edu>
2014-04-28 12:53:01 -0500

    Moved email config into yaml file.

--- config/initializers/example.setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.umn.edu",
-  :port                 => 587,
-  :user_name            => "<user name here>",
-  :domain               => "<domain here>",
-  :password             => "<your password here>",
-  :authentication       => :login,
-  :enable_starttls_auto => true
-}
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,15 @@
+config = YAML::load(File.open(Rails.root.join "config", "email.yml"))  
+if config && !config.empty?   
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.umn.edu",
+    :port                 => 587,
+    :user_name            => config["user"],
+    :domain               => config["domain"],
+    :password             => config["password"],
+    :authentication       => :login,
+    :enable_starttls_auto => true
+  }
+else
+  raise "You must provide an config/email.yml file to run this application. See config/email.yml.example for an example."
+end 
+
