de0e68688ec66e55f13cdb886d7568d015ccf160
David Peterson <pete2786@umn.edu>
2014-04-30 13:25:48 -0500

    Fixed issue with admin interface and default scopes not getting along.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   def to_json_table(collection, date)
     data_table = [['Date', 'N'].to_s]
     data_table << collection.select("count(*) as n, #{variable_date_truncate(date)} as day").
-    group(variable_date_truncate(date)).collect{|c| [c.day, c.n].to_s}
+    group(variable_date_truncate(date)).reorder("day").collect{|c| [c.day, c.n].to_s}
 
     return data_table.collect{|v| v}.join(",").html_safe
   end
