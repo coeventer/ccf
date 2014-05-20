ff45dc105181203e77e53b0b725499e1e4c99cd7
David Peterson <pete2786@umn.edu>
2014-05-19 22:26:42 -0500

    updates admin to fill in dates with no values for an event to show zeros.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,24 @@
 module ApplicationHelper
   def variable_date_truncate(date)
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2" then
-      "DATE_FORMAT(#{date}, '%c/%e/%Y')"
+      "DATE_FORMAT(#{date}, '%m/%d/%Y')"
     else
       "TO_CHAR(#{date}, 'MM/DD/YYYY')"
     end
   end
 
   # Returns a json object which can be loaded into a Google data table
-  def to_json_table(collection, date)
+  def to_json_table(event, collection, date)
+    dates_hash = Hash.new
+    (event.created_at.to_date..Date.today).map{ |date| date.strftime("%m/%d/%Y") }.each do |d| 
+      dates_hash[d]=0 
+    end
+    
+    collection.select("count(*) as n, #{variable_date_truncate(date)} as day").
+    group(variable_date_truncate(date)).reorder("day").each{|c| dates_hash[c.day]=c.n}
+    
     data_table = [['Date', 'N'].to_s]
-    data_table << collection.select("count(*) as n, #{variable_date_truncate(date)} as day").
-    group(variable_date_truncate(date)).reorder("day").collect{|c| [c.day, c.n].to_s}
+    data_table << dates_hash.to_a.collect{|c| c.to_s}
 
     return data_table.collect{|v| v}.join(",").html_safe
   end
