8b31d42b510956ee5fdbd2c394ef416aa8c529c4
Chad Fennell <fenne035@umn.edu>
2014-08-08 12:45:58 -0500

    domain to reflect the new site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ CampusCodefest::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "apps.oir.umn.edu/campus_codefest" }
+  config.action_mailer.default_url_options = { :host => "campuscodefest.org" }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
