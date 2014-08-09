f3035a723704317f2080442ab09306ff6b288060
Chad Fennell <fenne035@umn.edu>
2014-08-09 11:40:14 -0500

    make session store configurable so we can for e.g. do lvh.me on dev and campuscodefest on prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CampusCodefest::Application.config.session_store :cookie_store, key: '_campus_codefest_session', :domain => 'lvh.me'
+CampusCodefest::Application.config.session_store :cookie_store, key: '_campus_codefest_session', :domain => APP_CONFIG["domain"]
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
