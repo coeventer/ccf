170e6f71671b402016dc4ca4ef58ccbf3ad53daa
Chad Fennell <fenne035@umn.edu>
2014-08-09 11:38:42 -0500

    make production configurable for those who want to stand-up their own ccf service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ CampusCodefest::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "campuscodefest.org" }
+  config.action_mailer.default_url_options = { :host => APP_CONFIG["domain"] }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
