50a65451f62f7bbb2368b8b5dc5665b75d638b2d
David Peterson <pete2786@umn.edu>
2014-09-14 17:04:39 -0500

    scope for mixed controller.

--- app/controllers/organization_lib.rb
+++ app/controllers/organization_lib.rb
@@ -2,6 +2,7 @@ module OrganizationLib
   def self.included(base)
     base.before_filter :find_events
     base.helper_method :current_organization
+    base.around_filter :scope_current_organization, if: current_organization
   end
 
   def current_organization
@@ -11,6 +12,13 @@ module OrganizationLib
   def find_events
     @future_events = Event.live.where(["end_date >= ?", Date.today]) 
     @past_events = Event.live.where(["end_date < ?", Date.today]) 
+  end
+
+  def scope_current_organization
+    Organization.current_id = current_organization.id
+    yield
+  ensure
+    Organization.current_id = nil
   end
 
 end
