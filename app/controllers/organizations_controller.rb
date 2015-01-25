8472ca5712ff3f5c7477fee2942ecf457f789796
David Peterson <pete2786@umn.edu>
2015-01-24 20:10:11 -0600

    Do not require login to view orgs.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ApplicationController
+  skip_before_filter :auth_required, :only => [:index]
   def index
     @organizations = Organization.paginate(:page => params[:page], :per_page => 25)
   end
