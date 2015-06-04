250b747f4c902a88db889b3eb8f96a42d53638fa
David Peterson <pete2786@umn.edu>
2015-06-03 23:19:34 -0500

    Added reveal, need to reimplemented edit

--- app/assets/javascripts/app_reveal.js
+++ app/assets/javascripts/app_reveal.js
@@ -10,11 +10,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require bootstrap-wysihtml5
-//= require bootstrap-datepicker
-//= require forms
-//= require jquery.datetimepicker
-//= require projects
+//= require reveal
