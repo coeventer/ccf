078e5f63a94817a3849657239a1bacd3119486a4
David Peterson <david.peterson@sportngin.com>
2015-09-02 09:01:42 -0500

    Remove invitations step for now

--- app/controllers/admin/after_create_controller.rb
+++ app/controllers/admin/after_create_controller.rb
@@ -1,20 +1,14 @@
 class Admin::AfterCreateController < Admin::AdminController
   include Wicked::Wizard
-  steps :guide, :privacy, :logo, :events, :invitations, :done
+  steps :guide, :privacy, :logo, :events, :done
 
   def show
-    case step 
-    when :invitations
-      skip_step if current_organization.auto_verify
-    else
-    end
-
     render_wizard
   end
 
   def update
     skip_step if current_organization.update_attributes(params[:organization])
-    
+
     render_wizard
   end
 
