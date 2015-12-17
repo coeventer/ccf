7a8a0515ecaa06663e86212d5770089b1be0fc17
Bill Bushey <wbushey@gmail.com>
2015-12-17 12:56:23 -0600

    Adding migration to set all existing users to have email_confirmed = true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213171916) do
+ActiveRecord::Schema.define(version: 20151217184014) do
 
   create_table "event_comments", force: :cascade do |t|
     t.integer  "event_id"
