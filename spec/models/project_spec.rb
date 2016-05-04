1b85c624e2f640fb2ac61fe5690347c3e50496f9
Andy Fleener <andrew.fleener@sportngin.com>
2016-05-04 17:05:40 -0500

    Switch to using the project url in slack messages instead of the less useful description

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,91 +5,115 @@ describe Project do
   it "should require a title" do
     should validate_presence_of(:title)
   end
-  
+
   it "should require a description" do
     should validate_presence_of(:description)
   end
-  
+
   it "should allow a project owner or accept none" do
     should allow_value(nil).for(:project_owner)
     should allow_value(1).for(:project_owner_id)
   end
-  
+
   it "should not require a classification" do
-    should_not validate_presence_of(:classification)    
+    should_not validate_presence_of(:classification)
   end
 
   it "should have volunteers" do
     should have_many(:volunteers)
   end
-  
+
   it "should have comments" do
     should have_many(:volunteers)
   end
-  
+
   it "should have ratings" do
     should have_many(:ratings)
   end
-  
+
   it "should belong to an event" do
     should belong_to(:event)
   end
-  
+
   it "should have tags" do
     should have_many(:tags)
   end
-      
+
+  context "#slack_message" do
+    describe "with an event" do
+      subject { create(:project, organization: organization, title: "A Title") }
+      it "generates a nice looking slack message" do
+        allow(subject).to receive(:url) { "http://example.com" }
+        expect(subject.slack_message).to match("A new project A Title has been submitted to the backlog. http://example.com")
+      end
+    end
+    describe "without an event" do
+      let(:event) { create(:event,
+                           title: "Event Title",
+                           start_date: Date.today.advance(:days => 5),
+                           voting_enabled: true,
+                           voting_end_date: Date.tomorrow)
+      }
+      subject { create(:project, organization: organization, event: event, title: "A Title") }
+
+      it "generates a nice looking slack message" do
+        allow(subject).to receive(:url) { "http://example.com" }
+        expect(subject.slack_message).to match("A new project A Title has been submitted for Event Title. http://example.com")
+      end
+
+    end
+  end
   describe "that belongs to an event" do
     it "should allow votes if event accepts votes" do
       @event = create(:event, start_date: Date.today.advance(:days => 5),
                             voting_enabled: true,
                             voting_end_date: Date.tomorrow)
-                            
+
       @project = create(:project, organization: organization, event: @event)
-      
+
       expect(@project.voting_allowed?).to be true
     end
-    
+
     it "should not allow votes if event does not accept votes" do
       @event = create(:event, start_date: Date.today.advance(:days => 5),
                             voting_enabled: true,
                             voting_end_date: Date.today.advance(:days => -2))
-                            
+
       @project = create(:project, organization: organization, event: @event)
-      
+
       expect(@project.voting_allowed?).to be false
     end
-    
+
     it "should allow volunteers if event accepts volunteers" do
       @event = create(:event, end_date: Date.today.advance(:days => 5),
                             volunteering_enabled: true,
                             volunteer_end_date: Date.tomorrow)
-                            
+
       @project = create(:project, organization: organization, event: @event)
-      
+
       expect(@project.volunteering_allowed?).to be true
     end
-    
+
     it "should not allow volunteers if event accepts volunteers" do
       @event = create(:event, end_date: Date.today.advance(:days => 5),
                             volunteering_enabled: true,
                             volunteer_end_date: Date.yesterday)
-                            
+
       @project = create(:project, organization: organization, event: @event)
-      
+
       expect(@project.volunteering_allowed?).to be false
-    end    
+    end
   end
-  
+
   describe "that does not belong to an event" do
     it "should not accept votes" do
       @project = create(:project, organization: organization)
-      expect(@project.voting_allowed?).to be false      
+      expect(@project.voting_allowed?).to be false
     end
-    
+
     it "should not accept volunteers" do
       @project = create(:project, organization: organization)
-      expect(@project.volunteering_allowed?).to be false      
+      expect(@project.volunteering_allowed?).to be false
     end
   end
 
@@ -104,7 +128,7 @@ describe Project do
       @project = create(:project, organization: organization, :event => @event)
     end
 
-    it "should determine if a user has voted on a project" do 
+    it "should determine if a user has voted on a project" do
       expect(@project.voted_on?(@user)).to be false
       @project_rating = create(:project_rating, user: @user, project: @project)
       expect(@project.voted_on?(@user)).to be true
@@ -143,7 +167,7 @@ describe Project do
       expect(lambda{@project.unvolunteer(@user)}).to_not change(ProjectVolunteer, :count)
     end
   end
-  
+
   # Need to get knowledge of GitHub API here. Contact Debbie G at UMN CSE
   it "should associate a github account"
 end
