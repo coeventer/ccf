d05b816b44a74d3b6d3cd6b595173ae117793d8c
Andy Fleener <andrew.fleener@sportngin.com>
2016-05-04 17:05:02 -0500

    Fix spec deprecation warnings

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,26 +6,26 @@ describe Organization do
 
   it "should properly identify org admins" do
     create(:organization_user, user: user, organization: subject, admin: true)
-    subject.admin?(user).should == true
+    expect(subject.admin?(user)).to be true
   end
 
   it "should properly identify org non-admins" do
     create(:organization_user, user: user, organization: subject, admin: false)
-    subject.admin?(user).should == false
+    expect(subject.admin?(user)).to be false
   end
 
   it "should properly identify org verified members" do
     create(:organization_user, user: user, organization: subject, verified: true)
-    subject.verified?(user).should == true
+    expect(subject.verified?(user)).to be true
   end
 
   it "should properly identify org unverified members" do
     create(:organization_user, user: user, organization: subject, verified: false)
-    subject.verified?(user).should == false
+    expect(subject.verified?(user)).to be false
   end
 
   it "should properly identify org members" do
     create(:organization_user, user: user, organization: subject, verified: true)
-    subject.member?(user).should == true
+    expect(subject.member?(user)).to be true
   end
 end
