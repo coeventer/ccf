5466bace482fcef51553a590eeaa949de7d93e21
Andy Fleener <andrew.fleener@sportngin.com>
2016-05-05 10:15:01 -0500

    Set the default domain config option so url routing works

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 APP_CONFIG = YAML::load(File.open(Rails.root.join "config", "config.yml"))[Rails.env]
 
+Rails.application.routes.default_url_options[:host] = APP_CONFIG['domain']
+
 # Initialize the rails application
 CampusCodefest::Application.initialize!
