1667de15f6d03cdca477f34603e182750e612839
Andy Fleener <andrew.fleener@sportngin.com>
2016-05-05 10:14:13 -0500

    Add project url to project volunteer slack message

--- spec/models/project_volunteer_spec.rb
+++ spec/models/project_volunteer_spec.rb
@@ -5,8 +5,20 @@ describe ProjectVolunteer do
   it "should belong to a user"
   it "should notify the project owner when created"
   it "should notify the project owner when destroyed"
-  
+
   it "should be unique between a user and project"
   it "should not be allowed if project does not have an event"
-  it "should not be allowed if project event expired" 
+  it "should not be allowed if project event expired"
+
+  context "#slack_message" do
+    before do
+      ProjectVolunteer.skip_callback(:validate, :limit_projects)
+    end
+    let(:organization) { create :organization }
+    subject { create(:project_volunteer, user: build(:user, name: "Bob"), project: create(:project, organization: organization, title: "Project") ) }
+    it "generates a nice looking slack message" do
+      allow(subject.project).to receive(:url) { "http://example.com" }
+      expect(subject.slack_message).to match("Bob has volunteered for Project: http://example.com")
+    end
+  end
 end
